f26ea82ffab2886a066d0775f2c88681875feefd
fansha <julfanshahidayah@gmail.com>
2024-07-30 12:05:01 +0700

    fix(auth-service/test): auth.service.spec.ts

--- apps/auth-service/src/app/auth/auth.service.spec.ts
+++ apps/auth-service/src/app/auth/auth.service.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UnauthorizedException } from '@nestjs/common';
 import { CqrsModule, CommandBus, QueryBus } from '@nestjs/cqrs';
+import { createMock } from '@golevelup/ts-jest';
 
 import { AuthService } from './auth.service';
+import { UserRegisterCommand } from './commands/impl/user-register.command';
 import { GetUserLoginQuery } from './queries/impl/get-user-login.query';
 
-import type { IAuthLogin, IAuthMe } from 'shared/interfaces/Auth';
+import type { IAuthLogin, IAuthMe, IAuthRegister } from 'shared/interfaces/Auth';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let cmdBus: CommandBus;
   let queryBus: QueryBus;
 
   beforeAll(async () => {
@@ -20,38 +23,32 @@ describe('AuthService', () => {
         AuthService,
         {
           provide: CommandBus,
-          useValue: {
-            execute: jest.fn(),
-          },
+          useValue: createMock<CommandBus>(),
         },
         {
           provide: QueryBus,
-          useValue: {
-            execute: jest.fn(),
-          },
+          useValue: createMock<QueryBus>(),
         },
       ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    cmdBus = module.get<CommandBus>(CommandBus);
     queryBus = module.get<QueryBus>(QueryBus);
   });
 
   describe('login', () => {
     it('should return a user success login', async () => {
-      const mockBody = {
-        username: 'qytela',
-        password: '123123',
-      };
       const mockRes: IAuthLogin = {
         userId: 1,
         token: 'xxxxxxxxxxxx',
         roles: ['user'],
       };
 
-      jest.spyOn(queryBus, 'execute').mockResolvedValue(mockRes as never);
-      const result = await service.login(mockBody);
+      jest.spyOn(queryBus, 'execute').mockResolvedValue(mockRes);
+      const result = await queryBus.execute(new GetUserLoginQuery(1));
 
+      expect(queryBus.execute).toHaveBeenCalled();
       expect(result).toEqual(
         expect.objectContaining<IAuthLogin>({
           userId: expect.any(Number),
@@ -59,7 +56,6 @@ describe('AuthService', () => {
           roles: expect.any(Array),
         })
       );
-      expect(queryBus.execute).toHaveBeenCalledWith(new GetUserLoginQuery(1));
     });
 
     it('should return a user failed login', async () => {
@@ -69,10 +65,38 @@ describe('AuthService', () => {
       };
 
       try {
-        await service.login(mockBody);
+        const result = await service.login(mockBody);
+        expect(result).toHaveBeenCalled();
       } catch (error) {
         expect(error).toBeInstanceOf(UnauthorizedException);
       }
+    });
+  });
+
+  describe('register', () => {
+    it('should return a user success register', async () => {
+      const mockBody = {
+        name: 'Fansa',
+        username: 'fansa',
+        password: '123123',
+      };
+      const mockRes: IAuthRegister = {
+        name: 'Fansa',
+        username: 'fansa',
+        password: '123123',
+      };
+
+      jest.spyOn(cmdBus, 'execute').mockResolvedValue(mockRes);
+      const result = await cmdBus.execute(new UserRegisterCommand(mockBody));
+
+      expect(cmdBus.execute).toHaveBeenCalled();
+      expect(result).toEqual(
+        expect.objectContaining({
+          name: expect.any(String),
+          username: expect.any(String),
+          password: expect.any(String),
+        })
+      );
     });
   });
 
